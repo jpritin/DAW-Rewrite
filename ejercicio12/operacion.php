5e81eec4013bdf914a129fadf610c79e84a7c767
Hinestrosa <98031771+hinestrosa4@users.noreply.github.com>
2023-01-11 18:34:36 +0100

    Create operacion.php

---
+++ ejercicio12/operacion.php
@@ -0,0 +1,24 @@
+<?php
+    $op = $_GET["op"];
+    $op1 = $_GET["op1"];
+    $op2 = $_GET["op2"];
+
+    switch ($op) {
+        case "sumar":
+            $result = $op1 + $op2;
+            break;
+        case "restar":
+            $result = $op1 - $op2;
+            break;
+        case "multiplicar":
+            $result = $op1 * $op2;
+            break;
+        case "dividir":
+            $result = $op1 / $op2;
+            break;
+        default:
+            $result = "Operacion invalida";
+            break;
+    }
+    echo $result;
+?>
